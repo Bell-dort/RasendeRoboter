6300e18266e68e75a5e3102206404da93be35926
BaptistePDJ <65275664+BaptistePDJ@users.noreply.github.com>
2020-12-18 15:59:57 +0100

    Petite modification et ajouts

--- plaque.py
+++ plaque.py
@@ -5,21 +5,44 @@ class plaque(object):
 	def __init__(self, element):
 		self.element = element
 
+	switch90(self):
+		for i in range(8):
+			for j in range(8):
+				self.element[i][j].ToDroite()
 
-plaque1 = plaque(np.array([np.array[caseVide,caseVide,caseVide,caseVide,caseMurBas,caseMurHaut,caseMurDroit,caseVide],
-						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseMurBas,caseCarreJaune,caseVide],
-						   np.array[caseVide,caseVide,caseVide,caseMurBas,caseCercleVert,caseVide,caseVide,caseVide],
-						   np.array[caseMurDroit,caseVide,caseVide,caseVide,caseMurGauche,caseVide,caseVide,caseVide],
-						   np.array[caseMurGauche,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide],
-						   np.array[caseVide,caseVide,caseLosangeBleu,caseVide,caseVide,caseVide,caseVide,caseVide],
-						   np.array[caseVide,caseVide,caseMurGauche,caseVide,caseVide,caseMurDroit,caseVide,caseMurDroit],
-						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseTriangleRouge,caseMurHautBas,caseCentral]]));		
 
-plaque2 = plaque(np.array([np.array[caseVide,caseVide,caseVide,caseMurDroit,caseVide,caseMurHaut,caseMurBas,caseCentral],
-						   np.array[caseMurGauche,caseVide,caseMurBas,caseCarreBleu,caseVide,caseVide,caseVide,caseMurGauche],
-						   np.array[caseVide,caseVide,caseVide,caseMurBas,caseCercleVert,caseVide,caseVide,caseVide],
-						   np.array[caseMurDroit,caseVide,caseVide,caseVide,caseMurGauche,caseVide,caseVide,caseVide],
+plaque1 = plaque(np.array([np.array[caseVide,caseVide,caseVide,caseVide,caseMurBas,caseMurHaut,caseMurDroit,caseVide],              #0
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseMurBas,caseCarreJaune,caseVide],               #1
+						   np.array[caseVide,caseVide,caseVide,caseMurBas,caseCercleVert,caseVide,caseVide,caseVide],               #2
+						   np.array[caseMurDroit,caseVide,caseVide,caseVide,caseMurGauche,caseVide,caseVide,caseVide],              #3
+						   np.array[caseMurGauche,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide],                  #4
+						   np.array[caseVide,caseVide,caseLosangeBleu,caseMurHaut,caseVide,caseVide,caseVide,caseVide],             #5
+						   np.array[caseVide,caseVide,caseMurGauche,caseVide,caseVide,caseMurDroit,caseVide,caseMurDroit],          #6
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseTriangleRouge,caseMurHautBas,caseVide]]));     #7		
+
+plaque2 = plaque(np.array([np.array[caseVide,caseVide,caseVide,caseMurDroit,caseVide,caseVide,caseMurBas,caseVide],
+						   np.array[caseMurDroit,caseVide,caseMurBas,caseCarreBleu,caseVide,caseVide,caseVide,caseMurGauche],
 						   np.array[caseMurGauche,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide],
-						   np.array[caseVide,caseVide,caseLosangeBleu,caseVide,caseVide,caseVide,caseVide,caseVide],
-						   np.array[caseVide,caseVide,caseMurGauche,caseVide,caseVide,caseMurDroit,caseVide,caseVide],
-						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseTriangleRouge,caseVide,caseVide.ToCentre()]]));	
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide],
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseLosangeJaune,caseMurHaut],
+						   np.array[caseVide,caseTriangleVert,caseMurHaut,caseVide,caseVide,caseVide,caseVide,caseVide],
+						   np.array[caseVide,caseMurGauche,caseVide,caseMurBas,caseVide,caseVide,caseVide,caseVide],
+						   np.array[caseVide,caseMurBas,caseMurHaut,caseVide,caseMurGauche,caseVide,caseVide,caseVide]]));	
+
+plaque3 = plaque(np.array([np.array[caseVide,caseVide,caseMurBas,caseMurHaut,caseVide,caseVide,caseVide,caseVide],
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseMurBas,caseLosangeRouge,caseVide,caseVide],
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseMurGauche,caseVide,caseVide],
+						   np.array[caseVide,caseMurDroit,caseVide,caseVide,caseVide,caseVide,caseCarreVert,caseMurHaut],
+						   np.array[caseVide,caseTriangleJaune,caseMurHaut,caseVide,caseVide,caseVide,caseMurGauche,caseMurDroit],
+						   np.array[caseVide,caseVide,caseMurDroit,caseVide,caseVide,caseVide,caseVide,caseMurGauche],
+						   np.array[caseMurDroit,caseMurBas,caseCercleBleu,caseVide,caseVide,caseVide,caseVide,caseVide],
+						   np.array[caseVide,caseMurHaut,caseVide,caseMurBas,caseMulticouleur,caseVide,caseVide,caseVide]]));	
+
+plaque4 = plaque(np.array([np.array[caseVide,caseMurHaut,caseVide,caseVide,caseMurGauche,caseVide,caseVide,caseVide],
+						   np.array[caseMurGauche,caseVide,caseVide,caseCercleJaune,caseMurHaut,caseVide,caseMurDroit,caseVide],
+						   np.array[caseVide,caseVide,caseVide,caseMurGauche,caseVide,caseMurBas,caseLosangeVert,caseVide],
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseVide,caseMurDroit],
+						   np.array[caseVide,caseMurDroit,caseVide,caseVide,caseVide,caseVide,caseVide,caseMurGauche],
+						   np.array[caseVide,casetriangleBleu,caseMurHaut,caseVide,caseVide,caseVide,caseVide,caseVide],
+						   np.array[caseVide,caseVide,caseVide,caseVide,caseMurBas,caseCarreRouge,caseVide,caseVide],
+						   np.array[caseVide,caseVide,caseVide,caseMurBas,caseMurHaut,caseMurGauche,caseVide,caseVide]]));
--- plateau.py
+++ plateau.py
@@ -1,6 +1,19 @@
+from random import randint
+import numby as np
+import plaque
+
 class plateau(object):
 	plat = ' '
 	"""docstring for plateau"""
-	def __init__(self, plat):
+	def __init__(self, plaque1,plaque2,plaque3,plaque4):
+		for i in range(randint(0,3)):
+			plaque1.switch90()
+		for j in range(randint(0,3)):
+			plaque2.switch90()
+		for t in range(randint(0,3)):
+			plaque3.switch90()
+		for k in range(randint(0,3)):
+			plaque4.switch90()
+		
 		self.plat = plat
 		
