a3130717dc2a2bfc33ac750e111d1278f7f9ecbb
BaptistePDJ <65275664+BaptistePDJ@users.noreply.github.com>
2020-12-23 17:34:53 +0100

    Version sûrement finale
    Réalisation du plateau terminé avec un générateur aléatoire de celui-ci 
    Avec Changement de sens des cases des plaques

--- plateau.py
+++ plateau.py
@@ -1,10 +1,33 @@
-from random import randint
 import numby as np
 import plaque
-
+import random
 class plateau(object):
-	plat = ' '
+	liste = [0,1,2,3];
+	plat = np.array([np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
+					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '])])
+
 	"""docstring for plateau"""
+	elaborationPlateau(L):
+		for i in range(4):
+			listeChangement = [' ',' ',' ',' '];
+			choix = random.choice(L)
+			del L[choix];
+			listeChangement[i] = choix
+		L = listeChangement
 	def __init__(self, plaque1,plaque2,plaque3,plaque4):
 		for i in range(randint(0,3)):
 			plaque1.switch90()
@@ -14,6 +37,19 @@ class plateau(object):
 			plaque3.switch90()
 		for k in range(randint(0,3)):
 			plaque4.switch90()
-		
-		self.plat = plat
+
+		listePlaque = [plaque1,plaque2,plaque3,plaque4]
+		elaborationPlateau(liste)
+		for i in range(8):
+			for j in range(8):
+				plat[i][j] = listePlaque[liste[0]].[i][j]
+		for i in range(8,16):
+			for j in range(8):
+				plat[i][j] = listePlaque[liste[1]].[i][j]
+		for i in range(8):
+			for j in range(8,16):
+				plat[i][j] = listePlaque[liste[2]].[i][j]
+		for i in range(8,16):
+			for j in range(8,16):
+				plat[i][j] = listePlaque[liste[3]].[i][j]
 		
