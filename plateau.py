e69a06e5866554432f112970aa5f3f0ce09d37c4
BaptistePDJ <65275664+BaptistePDJ@users.noreply.github.com>
2020-12-24 10:44:06 +0100

    Rajout de l'implémentation des cases Centrales et commentaires

--- plateau.py
+++ plateau.py
@@ -1,6 +1,7 @@
 import numby as np
 import plaque
 import random
+import case
 class plateau(object):
 	liste = [0,1,2,3];
 	plat = np.array([np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
@@ -20,7 +21,7 @@ class plateau(object):
 					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']),
 					 np.array([' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '])])
 
-	"""docstring for plateau"""
+	#Défini quelle plaque aura quelle position aléatoirement
 	elaborationPlateau(L):
 		for i in range(4):
 			listeChangement = [' ',' ',' ',' '];
@@ -28,7 +29,9 @@ class plateau(object):
 			del L[choix];
 			listeChangement[i] = choix
 		L = listeChangement
+
 	def __init__(self, plaque1,plaque2,plaque3,plaque4):
+		#Change l'orientation des plaques aléatoirement entre 0 et 3 fois
 		for i in range(randint(0,3)):
 			plaque1.switch90()
 		for j in range(randint(0,3)):
@@ -40,16 +43,20 @@ class plateau(object):
 
 		listePlaque = [plaque1,plaque2,plaque3,plaque4]
 		elaborationPlateau(liste)
+		#Défini le plateau avec chaque plaque aléatoire
 		for i in range(8):
 			for j in range(8):
-				plat[i][j] = listePlaque[liste[0]].[i][j]
+				self.plat[i][j] = listePlaque[liste[0]].[i][j]
 		for i in range(8,16):
 			for j in range(8):
-				plat[i][j] = listePlaque[liste[1]].[i][j]
+				self.plat[i][j] = listePlaque[liste[1]].[i][j]
 		for i in range(8):
 			for j in range(8,16):
-				plat[i][j] = listePlaque[liste[2]].[i][j]
+				self.plat[i][j] = listePlaque[liste[2]].[i][j]
 		for i in range(8,16):
 			for j in range(8,16):
-				plat[i][j] = listePlaque[liste[3]].[i][j]
-		
+				self.plat[i][j] = listePlaque[liste[3]].[i][j]
+		#Défini les cases central au centre du plateau
+		for i in range(7,8):
+			for j in range(7,8):
+				self.plat[i][j] = caseCentral
