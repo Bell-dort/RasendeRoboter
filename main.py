4e789d0b1e4a78649226d552cb689b4dd5b9ac0c
BaptistePDJ <65275664+BaptistePDJ@users.noreply.github.com>
2020-12-30 17:51:23 +0100

    Création d'une mini interface console pour démarrer un Jeu

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 #j'ai juste créé un main même si y a rien dedans pour l'instant
+import affichage
+import tkinter
+import time
+import plateau
+class Jeu(object):
+    nbJoueur = 1
+    difficulty = 1
+
+    def __init__(self,nbJoueur,difficulty):
+        self.nbJoueur = nbJoueur
+        self.difficulty = difficulty
+        for i in range(0, 16):
+            for j in range(0, 16):
+                # DrawCase(fenetre, plaque.plaque1.element[j][i]).grid(row = i, column = j)
+                affichage.DrawCase(affichage.fenetre, plateau.plateauJeu.plat[j][i]).grid(row=i, column=j)
+
+        affichage.fenetre.pack(expand=True)
+        affichage.window.mainloop()
+
+print("Bonjour Bienvenue dans le Jeu Rasende Roboter")
+print("Combien de joueur êtes vous ? ")
+nbJoueur = input("Nombre de joueur:")
+print("Quel niveau de difficulté:")
+print("Entre:")
+print("1 : Facile")
+print("2 : normal")
+print("3 : difficile")
+difficulty = input("Niveau de difficulté:")
+jeu = Jeu(nbJoueur,difficulty)
